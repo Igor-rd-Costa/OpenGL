a135324cdc714d2ecb08688232e1161297adc619
Igor-rd-Costa <115059901+Igor-rd-Costa@users.noreply.github.com>
2023-05-08 20:10:44 -0300

    Update UniformBuffer.cpp

--- Core/src/OpenGL/UniformBuffer.cpp
+++ Core/src/OpenGL/UniformBuffer.cpp
@@ -3,11 +3,25 @@
 
 namespace Core {
 
-UniformBuffer::UniformBuffer(void* dataStruct)
-{
-glCreateBuffer(1, &m_Id);
- gl_NamedBufferData(m_Id, size, dataStruct, GL_DYNAMIC_DRAW);
-}
+	UniformBuffer::UniformBuffer(void* dataStruct)
+	{
+		glCreateBuffer(1, &m_Id);
+		gl_NamedBufferData(m_Id, size, dataStruct, GL_DYNAMIC_DRAW);
+	}
+    
+	void UniformBuffer::Bind(uint32_t index)
+	{
+
+	}
+	
+	
+    template<>
+    void UniformBuffer::SetData<glm::vec3>(glm::vec3 data)
+	{
+		
+		
+		
+	}
 
 
 
