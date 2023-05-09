a601ecda9f2b9eba2557a2525ce5a43fc650f62f
Igor-rd-Costa <115059901+Igor-rd-Costa@users.noreply.github.com>
2023-05-09 19:04:34 -0300

    Update UniformBuffer.h

--- Core/src/OpenGL/UniformBuffer.h
+++ Core/src/OpenGL/UniformBuffer.h
@@ -10,12 +10,14 @@ class UniformBuffer {
 	~UniformBuffer();
 	
 	void Bind(uint32_t index) const;
+	void Link(uint32_t programId, const std::string& uniformBlock) const;
 	
 	template<typename T>
-	void SetData(T data);
+	void SetData(const std::string& uniform, T data);
 	
 	private:
 	uint32_t m_Id;
+	uint32_t m_BindingPoint;
 	static uint32_t s_MaxUniformBufferBindings;
 	static uint32_t s_BoundBuffers;
 }
